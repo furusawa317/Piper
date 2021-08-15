92421c1a06314d87edcbd681e05b9bb7710ad780
furusawa317 <takahirofurusawa@hotmail.com>
2021-08-15 15:09:57 +0900

    08151509

--- Flask/fukai.py
+++ Flask/fukai.py
@@ -12,7 +12,7 @@ humid_array = np.array(scraping.humid, dtype=float)
 
 ## 不快指数の計算
 # 不快指数＝0.81×温度+0.01×湿度x（0.99×温度－14.3）+46.3
-f_index_array = 0.81 * temp_array + 0.01 * humid_array * (0.99 * temp_array - 14.3) +46.3
+f_index_array = 0.81 * temp_array + 0.01 * humid_array * (0.99 * temp_array - 14.3) + 46.3
 #print(f_index_array)
 
 # 不快指数のリスト化
